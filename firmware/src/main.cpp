65539d8d951165b492c0e65451ac08015541f979
Victor Tseng <palatis@gmail.com>
2017-01-17 05:12:21 +0800

    [fw] read the data as fast as you can...
    and spit out to Serial when input changed.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -15,7 +15,7 @@
 
 #include "WreckedSPI.h"
 
-WreckedSPI<7, 2, 3, 8> spi;
+WreckedSPI<7, 2, 8, 3> spi;
 
 DigitalPin<4> latchPinOut; // for 74HC595
 DigitalPin<9> latchPinIn;  // for 74HC165
@@ -23,9 +23,12 @@ DigitalPin<9> latchPinIn;  // for 74HC165
 union {
     uint8_t bytes[4];
     uint32_t integer;
-} data;
+} out;
 
 uint8_t in[3] = { 0 };
+uint8_t cur_in[3] = { 0 };
+
+uint32_t last_diag_millis;
 
 void setup() {
     // for the code to run, hit C-d in the serial console ti de-activate DTR.
@@ -42,41 +45,60 @@ void setup() {
     spi.begin();
     digitalWrite(5, HIGH); // cleared 74HC595, put it back on.
 
-    data.integer = 1;
+    out.integer = 1;
+
+    last_diag_millis = millis();
 }
 
 void loop() {
-    Serial.print(millis());
-    Serial.print(": out = ");
-    Serial.print((int) (data.bytes[0]), HEX);
-    Serial.print(" ");
-    Serial.print((int) (data.bytes[1]), HEX);
-    Serial.print(" ");
-    Serial.print((int) (data.bytes[2]), HEX);
-    Serial.print(", in = ");
-
     uint32_t t1 = micros();
     latchPinOut = LOW;
     latchPinIn = HIGH;
-    in[0] = spi.transfer(data.bytes[0]);
-    in[1] = spi.transfer(data.bytes[1]);
-    in[2] = spi.transfer(data.bytes[2]);
+    in[0] = spi.transfer(out.bytes[0]);
+    in[1] = spi.transfer(out.bytes[1]);
+    in[2] = spi.transfer(out.bytes[2]);
     latchPinOut = HIGH;
     latchPinIn = LOW;
+
+    bool do_print = false;
+    if (in[0] != cur_in[0] || in[1] != cur_in[1] || in[2] != cur_in[2]) {
+        cur_in[0] = in[0];
+        cur_in[1] = in[1];
+        cur_in[2] = in[2];
+        do_print = true;
+    }
     uint32_t t2 = micros();
 
-    Serial.print((int) (in[0]), HEX);
-    Serial.print(" ");
-    Serial.print((int) (in[1]), HEX);
-    Serial.print(" ");
-    Serial.print((int) (in[2]), HEX);
-    Serial.print(", took = ");
+    delayMicroseconds(50);
 
-    Serial.print(t2 - t1);
-    Serial.println("us");
+    if (do_print) {
+        Serial.print(millis());
+        Serial.print(": in = ");
+        Serial.print((int) (in[0]), HEX);
+        Serial.print(" ");
+        Serial.print((int) (in[1]), HEX);
+        Serial.print(" ");
+        Serial.print((int) (in[2]), HEX);
+        Serial.print(", took = ");
+        Serial.print(t2 - t1);
+        Serial.println("us");
+    }
 
-    data.integer <<= 1;
-    if (data.integer == 1l << 24)
-        data.integer = 1;
-    delay(500);
+    if (millis() - last_diag_millis > 500) {
+        last_diag_millis = millis();
+        out.integer <<= 1;
+        if (out.integer == 1l << 24)
+            out.integer = 1;
+
+        Serial.print(millis());
+        Serial.print(": out = ");
+        Serial.print((int) (out.bytes[0]), HEX);
+        Serial.print(" ");
+        Serial.print((int) (out.bytes[1]), HEX);
+        Serial.print(" ");
+        Serial.print((int) (out.bytes[2]), HEX);
+        Serial.print(", took = ");
+        Serial.print(t2 - t1);
+        Serial.println("us");
+    }
 }
