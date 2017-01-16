7c3697bdb8e622aedec7a9fef372fa95241286f5
Victor Tseng <palatis@gmail.com>
2017-01-17 04:28:24 +0800

    [fw] do send & receive at the same time...
    do both send & receive at the same time, and see how long it takes.
    
    ~28~36us monitored due to micros() resolution, the actual time
    should be around 22us.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -13,14 +13,19 @@
 #include <Arduino.h>
 #include <DigitalIO.h>
 
-DigitalPin<3> clockPin;
-DigitalPin<2> dataPin;
-DigitalPin<4> latchPin;
+#include "WreckedSPI.h"
+
+WreckedSPI<7, 2, 3, 8> spi;
+
+DigitalPin<4> latchPinOut; // for 74HC595
+DigitalPin<9> latchPinIn;  // for 74HC165
 
 union {
     uint8_t bytes[4];
     uint32_t integer;
 } data;
+
+uint8_t in[3] = { 0 };
 
 void setup() {
     // for the code to run, hit C-d in the serial console ti de-activate DTR.
@@ -32,55 +37,40 @@ void setup() {
 
     digitalWrite(5, LOW); // clear 74HC595 memories (toggle it on lator)
     digitalWrite(6, LOW); // enable 74HC595
-    latchPin.config(OUTPUT, HIGH); // latchPin in output mode with initial level HIGH.
-    clockPin.config(OUTPUT, LOW); // clockPin in output mode with initial level LOW.
-    dataPin.config(OUTPUT, HIGH); // dataPin in output mode with initial level HIGH.
+    latchPinOut.config(OUTPUT, HIGH); // latchPinOUT in output with initial HIGH
+    latchPinIn.config(OUTPUT, LOW); // latchPinIn in input with initial LOW
+    spi.begin();
     digitalWrite(5, HIGH); // cleared 74HC595, put it back on.
 
     data.integer = 1;
 }
 
-//------------------------------------------------------------------------------
-// Time to send one bit is ten cycles or 625 ns for 16 MHz CPU.
-inline __attribute__((always_inline))
-void sendBit(uint8_t bit, uint8_t data) {
-  dataPin = data & (1 << bit);
-  clockPin = 1;
-  // may want a nop here - clock pulse is 125 ns wide
-  clockPin = 0;
-}
-//------------------------------------------------------------------------------
-// Time to send one byte is 5 usec.
-void shiftOut(uint8_t bits) {
-  sendBit(7, bits);
-  sendBit(6, bits);
-  sendBit(5, bits);
-  sendBit(4, bits);
-  sendBit(3, bits);
-  sendBit(2, bits);
-  sendBit(1, bits);
-  sendBit(0, bits);
-}
-
 void loop() {
     Serial.print(millis());
-    Serial.print(": ");
+    Serial.print(": out = ");
     Serial.print((int) (data.bytes[0]), HEX);
     Serial.print(" ");
     Serial.print((int) (data.bytes[1]), HEX);
     Serial.print(" ");
     Serial.print((int) (data.bytes[2]), HEX);
-    Serial.print(" ");
-    Serial.print((int) (data.bytes[3]), HEX);
-    Serial.print(", took = ");
+    Serial.print(", in = ");
 
     uint32_t t1 = micros();
-    latchPin = LOW;
-    shiftOut(data.bytes[0]);
-    shiftOut(data.bytes[1]);
-    shiftOut(data.bytes[2]);
-    latchPin = HIGH;
+    latchPinOut = LOW;
+    latchPinIn = HIGH;
+    in[0] = spi.transfer(data.bytes[0]);
+    in[1] = spi.transfer(data.bytes[1]);
+    in[2] = spi.transfer(data.bytes[2]);
+    latchPinOut = HIGH;
+    latchPinIn = LOW;
     uint32_t t2 = micros();
+
+    Serial.print((int) (in[0]), HEX);
+    Serial.print(" ");
+    Serial.print((int) (in[1]), HEX);
+    Serial.print(" ");
+    Serial.print((int) (in[2]), HEX);
+    Serial.print(", took = ");
 
     Serial.print(t2 - t1);
     Serial.println("us");
