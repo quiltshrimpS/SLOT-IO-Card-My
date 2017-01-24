4670102dc8187d9e29b34146aefd7ffe2664668b
Victor Tseng <palatis@gmail.com>
2017-01-24 10:00:47 +0800

    [fw] implement EjectTimeoutTracker

---
+++ firmware/src/EjectTimeoutTracker.h
@@ -0,0 +1,46 @@
+#ifndef __EJECT_TIMEOUT_TRACKER_H__
+#define __EJECT_TIMEOUT_TRACKER_H__
+
+#include <Arduino.h>
+
+extern CmdMessenger messenger;
+
+class EjectTimeoutTracker {
+public:
+	EjectTimeoutTracker():
+		_check(false)
+	{
+	}
+
+	__attribute__((always_inline)) inline
+	void begin(uint32_t const timeout_us) {
+		_timeout_us = timeout_us;
+	}
+
+	__attribute__((always_inline)) inline
+	void start(uint32_t const now = micros()) {
+		_start_us = now;
+		_check = true;
+	}
+
+	__attribute__((always_inline)) inline
+	void stop() {
+		_check = false;
+	}
+
+	__attribute__((always_inline)) inline
+	bool trigger(uint32_t const now = micros()) {
+		if (_check && now - _start_us > _timeout_us) {
+			_check = false;
+			return true;
+		}
+		return false;
+	}
+
+private:
+	uint32_t _timeout_us;
+	uint32_t _start_us;
+	bool _check;
+};
+
+#endif
